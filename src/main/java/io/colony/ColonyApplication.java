499efb9b6e8e0e4845f588eeb296ef49d930c0ad
Alan <alan.souza.fagundes@gmail.com>
2018-11-10 21:50:27 -0300

    First Commit

---
+++ src/main/java/io/colony/ColonyApplication.java
@@ -0,0 +1,12 @@
+package io.colony;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColonyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColonyApplication.class, args);
+	}
+}
---
+++ src/test/java/io/colony/ColonyApplicationTests.java
@@ -0,0 +1,16 @@
+package io.colony;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ColonyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
